1d56cfb22380962ba611dc67b8492a5e06ce409a
Vaibhav Dongre <21vaibhavdongre@gmail.com>
2025-08-16 16:48:52 +0530

    Entities created

---
+++ app/src/main/java/ticket/booking/entities/Ticket.java
@@ -0,0 +1,20 @@
+package ticket.booking.entities;
+
+import java.util.Date;
+import java.util.SplittableRandom;
+
+public class Ticket {
+
+    private String ticketId;
+
+    private String userId;
+
+    private String source;
+
+    private String destination;
+
+    private Date dateOfTravel;
+
+    private Train train;
+
+}
